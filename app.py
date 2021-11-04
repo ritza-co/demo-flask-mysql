de9ee1b00533689043ec7ff34d3bf86e83b858d3
AnesuMurakata <murac96@gmail.com>
2021-11-04 14:29:45 +0200

    Modified db config steps

--- app.py
+++ app.py
@@ -3,13 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-
 # db variable initialization
 db = SQLAlchemy(app)
 
-migrate = Migrate(app, db)
+app = Flask(__name__, instance_relative_config=False)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_ECHO"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()  # Create sql tables for our data models
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
